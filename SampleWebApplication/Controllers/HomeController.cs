c79aed3882a7ad4f4654c9f3815733e728ae579e
Ilya Grebeshkov <grbeshkoff@gmail.com>
2015-06-17 00:06:14 +0300

    Update EF model and Connections

--- SampleWebApplication/Controllers/HomeController.cs
+++ SampleWebApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleWebApplication.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,8 @@ namespace SampleWebApplication.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            LittlenBDEntities productsContext = new LittlenBDEntities();
+            
             return View();
         }
     }
